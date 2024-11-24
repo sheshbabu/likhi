fc7117e7b7fd711e23a72d7f411fb69d9fe42bd2
Sheshbabu <sheshbabu@gmail.com>
2024-11-24 18:59:56 +0800

    Add initial implementation of markdown editor with custom CSS reset and styling

---
+++ assets/index.js
@@ -0,0 +1,15 @@
+function render() {
+    const md = window.markdownit({
+        highlight: function (str, lang) {
+            if (lang && window.hljs.getLanguage(lang)) {
+                try {
+                    return window.hljs.highlight(lang, str).value;
+                } catch (__) { }
+            }
+            return '';
+        }
+    });
+    const text = document.querySelector('.note-input').value;
+    const html = md.render(text);
+    document.querySelector('.note-output').innerHTML = html;
+}
