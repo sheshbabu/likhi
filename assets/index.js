c80b4b40510e4a967b439ca7a219f13d947a97da
Sheshbabu <sheshbabu@gmail.com>
2025-01-25 17:01:31 +0800

    Made typography tweaks
    * Using proper type scale
    * Removed SNPro fonts
    * Made the editor side-by-side

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
 function render() {
     const md = window.markdownit({
+        linkify: true,
         highlight: function (str, lang) {
             if (lang && window.hljs.getLanguage(lang)) {
                 try {
@@ -13,3 +14,5 @@ function render() {
     const html = md.render(text);
     document.querySelector('.note-output').innerHTML = html;
 }
+
+document.addEventListener('DOMContentLoaded', render);
